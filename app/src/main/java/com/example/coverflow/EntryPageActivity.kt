2c5ace01296f920bf96e62c1fd57fbaf85a943e3
sachinsolanki <sachin.solanki1111@gmail.com>
2023-08-02 13:13:45 +0530

    Added Labled Spinner

--- app/src/main/java/com/example/coverflow/EntryPageActivity.kt
+++ app/src/main/java/com/example/coverflow/EntryPageActivity.kt
@@ -2,24 +2,28 @@ package com.example.coverflow
 
 import android.content.Intent
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import java.util.Locale
+
 
 class EntryPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entry_page)
         val cardBtn =findViewById<Button>(R.id.card)
-        val spnLang =findViewById<Spinner>(R.id.spnLang)
+        val spnLang =findViewById<LabeledSpinner>(R.id.spnLang)
+
+        val personNames = arrayOf("EN", "AR")
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.select_dialog_item, personNames)
+        spnLang.setItem(arrayAdapter)
 
         cardBtn.setOnClickListener{
             val configuration = Configuration()
-            configuration.setLocale(Locale(spnLang.selectedItem.toString()))
+            configuration.setLocale(Locale(spnLang.toString()))
             resources.updateConfiguration(configuration, resources.displayMetrics)
-
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
---
+++ app/src/main/java/com/example/coverflow/LabeledSpinner.kt
@@ -0,0 +1,46 @@
+package com.example.coverflow
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.TextView
+import com.example.coverflow.R.styleable.labeledTextViewStyle
+
+
+class LabeledSpinner @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    LinearLayout(context, attrs, defStyleAttr) {
+
+    private lateinit var spnValue: Spinner
+    private lateinit var label: TextView
+    private lateinit var outHint: TextView
+
+    init {
+        init(attrs)
+    }
+
+
+    private fun init(attrs: AttributeSet?) {
+        View.inflate(context, R.layout.labeled_spinner, this)
+        spnValue = findViewById(R.id.spnValue)
+        label = findViewById(R.id.labelValue)
+        outHint = findViewById(R.id.out_hint)
+        val ta = context.obtainStyledAttributes(attrs, labeledTextViewStyle)
+        try {
+            label.text = ta.getString(R.styleable.labeledTextViewStyle_labelValue)
+        } finally {
+            ta.recycle()
+        }
+    }
+
+    fun setItem(arrayAdapter: ArrayAdapter<String>) {
+        spnValue.adapter = arrayAdapter
+    }
+}
